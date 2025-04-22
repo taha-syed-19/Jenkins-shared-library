a88ec8f8cb5c7ad4359b300d3b913c2353c1d1c8
taha-syed-19 <tahas752@gmail.com>
2025-04-22 16:03:41 +0530

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,9 +1,9 @@
 #!/usr/bin/env groovy
 def call(String imageName) {
     echo "Building the Docker Image in branch $BRANCH_NAME"
-    withCredentials([usernamePassword(credentialsId: 'Docker-Auth', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    withCredentials([usernamePassword(credentialsId: 'Docker-Auth', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
         sh "docker build -t $imageName ."
-        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
         sh "docker push $imageName"
     }
 }
